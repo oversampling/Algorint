31d3990ef16ea1ef92a7b111af98988d0394e121
chan1992241 <chan1992241@gmail.com>
2023-02-14 00:25:50 +0800

    CEE cpp test program

---
+++ test/CEE/code/cpp/cpp_logical_error.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void solve(string str, int strlen, int noOftestCase){
+    int counter = 1;
+    cout << "Case #" << noOftestCase +1<< ": ";
+    for (int i = 0; i < strlen - 1; i++){
+        cout << counter << " ";
+        if (str[i] < str[i+1]){
+            counter++;
+        }else counter = 1;
+    }
+    cout << counter + 1<< endl;
+}
+
+int main(){
+    int noOfTestCase, strlengthHold;
+    string strHold;
+    vector<int> strlength;
+    vector<string> str;
+    cin >> noOfTestCase;
+    cin.ignore();
+    for (int i =0 ;i<noOfTestCase; i++){
+        cin >> strlengthHold;
+        cin.ignore();
+        strlength.push_back(strlengthHold);
+        cin >> strHold;
+        cin.ignore();
+        str.push_back(strHold);
+        solve(str[i], strlength[i], i);
+    }
+}
