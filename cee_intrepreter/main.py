6b0eea1910549bf1b7629584292d92f92f453baa
chan1992241 <chan1992241@gmail.com>
2023-02-21 14:54:16 +0800

    Refactor with dind

--- cee_intrepreter/main.py
+++ cee_intrepreter/main.py
@@ -1,197 +1,303 @@
 #!/usr/bin/env python
+import io
+import tarfile
+from typing import Literal
 import pika
 import time
 import json
-import subprocess
 import os
 from dotenv import load_dotenv
 from redis.sentinel import Sentinel
 import time
 import redis
 import requests
+import docker
 
 
-def inject_username_password_to_rabbitmq_url(rabbitmq_url):
-    username = os.getenv("RABBITMQ_USERNAME").strip()
-    password = os.getenv("RABBITMQ_PASSWORD").strip()
-    rabbitmq_url = rabbitmq_url.replace(
-        "amqps://", "amqps://{}:{}@".format(username, password))
-    return rabbitmq_url
+class Sandbox:
+    def __init__(self, image: str, dind:docker.DockerClient, command: str, workdir = "/app", cpu_period: int = 1000000, mem_limit: str = "100m", pids_limit: int = 500):
+        self.image = image
+        self.dind = dind
+        self.container = self.dind.containers.create(
+            self.image,
+            command,
+            working_dir=workdir,
+            stdin_open=True,
+            tty=False,
+            detach=True,
+            cpu_period=cpu_period,
+            pids_limit=pids_limit,
+            mem_limit=mem_limit,
+            volumes={f'{workdir}/code': {'bind': workdir, 'mode': 'ro'}})
+        self.container.start()
+        self.stdin = self.container.attach_socket(params={'stdin': 1, 'stdout': 0, 'stderr': 0, 'stream': 1})
+        self.stdin._sock.setblocking(0)
+        self.stdin._writing = True
 
+    def write(self, data):
+        self.stdin.write(data.encode("utf-8"))
+        self.__wait(timeout=2)
 
-def initialize():
-    redis_master, channel, connection = None, None, None
-    if (os.getenv("ENVIRONMENT") == "development"):
-        redis_sentinels = os.getenv("REDIS_SENTINELS").strip()
-        redis_master_name = os.environ.get('REDIS_MASTER_NAME').strip()
-        redis_password = os.environ.get('REDIS_PASSWORD').strip()
-        submission_queue = os.getenv("SUBMISSION_QUEUE").strip()
-        redis_sentinel = Sentinel([(redis_sentinels, 5000)], socket_timeout=5)
-        redis_master = redis_sentinel.master_for(
-            redis_master_name, password=redis_password, socket_timeout=5)
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=submission_queue))
-        channel = connection.channel()
-
-    elif (os.getenv("ENVIRONMENT") == "production"):
-        rabbitmq_url = inject_username_password_to_rabbitmq_url(
-            os.getenv("SUBMISSION_QUEUE").strip())
-        parameters = pika.URLParameters(rabbitmq_url)
-        connection = pika.BlockingConnection(parameters)
-        channel = connection.channel()
-        redis_master = redis.Redis(
-            host=os.getenv("REDIS_HOST").strip(), port=6379)
-
-    return redis_master, channel, connection
-
-
-load_dotenv()
-redis_master, queue_channel, rabbitmq_connection = initialize()
-
-
-def execute_command(command, *args):
-    max_retries = 2
-    count = 0
-    backoffSeconds = 2
-    while True:
+    def __wait(self, timeout: int = 2):
         try:
-            return command(*args)
-        except (redis.exceptions.ConnectionError, redis.exceptions.TimeoutError):
-            count += 1
-            if count > max_retries:
-                raise
-        print('Retrying in {} seconds'.format(backoffSeconds))
-        time.sleep(backoffSeconds)
+            self.container.wait(timeout=timeout)
+        except Exception:
+            raise
+
+    def output(self):
+        return self.container.logs(stdout=True, stderr=False), self.container.logs(stdout=False, stderr=True)
+
+    @staticmethod
+    def _make_archive(filename: str, data: bytes):
+        tarstream = io.BytesIO()
+        tar = tarfile.open(fileobj=tarstream, mode='w')
+        tarinfo = tarfile.TarInfo(name=filename)
+        tarinfo.size = len(data)
+        tarinfo.mtime = int(time.time())
+        tar.addfile(tarinfo=tarinfo, fileobj=io.BytesIO(data))
+        tar.close()
+        tarstream.seek(0)
+        return tarstream
+
+    def add_file(self, filename: str, data: str) -> None:
+        tarstream = self._make_archive(filename, data.encode('utf-8'))
+        self.dind.api.put_archive(self.container.id, WORK_DIR, tarstream)
+
+    def status(self) -> dict[Literal["Status", "Running", "Paused", "Restarting",
+                                     "OOMKilled", "Dead", "Pid", "ExitCode", "Error", "StartedAt", "FinishedAt"]]:
+        self.container.reload()
+        return self.container.attrs['State']
+
+    def __del__(self):
+        self.container.remove(force=True)
+        self.stdin.close()
 
 
-def callback(ch, method, properties, body):
-    """
-    body attribute is JSON object with schema shown below
-    {
-        "langauage": <programming language: str>,
-        "code": <user code: str>,
-        "input": [<stdin stream: str>]
-        "test_cases": [expected output: str]
-    }
-    """
-    print(" [x] Received %r" % body.decode())
-    data = json.loads(body.decode())
-    # Save the code, input to a file
-    save_code(data["code"], data["language"])
-    # handle multiple input file
-    submission = {
-        "stdout": [],
-        "stderr": [],
-        "test_cases": data["test_cases"]
-    }
-    for code_jnput in data["input"]:
-        save_input(code_jnput)
-        # Execute the code
-        execute(data["language"])
-        submission["stderr"].append(
-            read_file("stderr.txt"))
-        submission["stdout"].append(
-            read_file("stdout.txt"))
-        # Clean Up stderr and stdout file
-        cleanup_stdout()
-        cleanup_stderr()
-    submission["status"] = "done execution"
-    # Save the result to submission database
-    execute_command(redis_master.set,
-                    data["submission_id"], json.dumps(submission), 600)
-    # Send the output to judge
-    judge(data["submission_id"])
-    # Clean up
-    clean_up(data["language"])
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+class Worker():
+    def __init__(self, envrionment ,**languages):
+        self.languages = languages
+        self.redis = self.__init_redis(envrionment)
+        self.queue = self.__init_rabbitmq(envrionment)
+        self.dind = self.__dind()
+        self.workdir = os.getenv("WORK_DIR").strip()
+        self.__load_interpreter(self.dind, self.languages)
+
+    def run(self, queue_name: str):
+        self.queue.basic_qos(prefetch_count=1)
+        self.queue.queue_declare(queue=queue_name.strip(), durable=True)
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        self.queue.basic_consume(queue=queue_name, on_message_callback=self.__callback)
+        self.queue.start_consuming()
+
+    def save_code(self, code: str, language: str):
+        """
+        Save the code to a file
+
+        Args:
+            code:
+                The code to be saved
+            language:
+                The language of the code
+        Returns:
+            None
+        Raises:
+            None
+        """
+        if (language == "python"):
+            with open("code/code.py", "w") as f:
+                f.write(code)
+        elif (language == "nodejs"):
+            with open("code/code.js", "w") as f:
+                f.write(code)
+        else:
+            print("Language not supported")
+
+    def read_file(self, filename):
+        with open(filename, "r") as f:
+            return f.read()
+
+    def __execute(self, language):
+        if (language == "python"):
+            sandbox = Sandbox(self.languages[language], self.dind, command="python code.py")
+        elif (language == "nodejs"):
+            sandbox = Sandbox(self.languages[language], self.dind, command="node code.js")
+        else:
+            return "", "Language not supported"
+        # Pass input to stdin
+        try:
+            sandbox.write(self.read_file(f'{WORK_DIR}/code/input.txt'))
+        except Exception as e:
+            print(sandbox.status())
+            return "", e.__str__()
+        stdout, stderr = sandbox.output()
+        del sandbox
+        return stdout, stderr
+
+    def __save_input(self, input: str):
+        """
+        Save the input to a file
+
+        Args:
+            input:
+                The input to be saved
+        Returns:
+            None
+        Raises:
+            None
+        """
+        with open("code/input.txt", "w") as f:
+            f.write(input)
+
+    def __clean_up(self, langauge):
+        os.remove("code/input.txt")
+        if (langauge == "nodejs"):
+            os.remove("code/code.js")
+        elif (langauge == "python"):
+            os.remove("code/code.py")
+        else:
+            print("Language not supported")
+
+    def __callback(self, ch, method, properties, body):
+        """
+        Execute after receiving a message from the queue
+
+        Args:
+            body:
+                The message body. It is a JSON string that contains the following fields:
+                    - code: The code to be executed
+                    - language: The language of the code
+                    - input: The input to the code
+                    - test_cases: The test cases to be executed
+                    - submission_id: The id of the submission
+        Returns:
+            None
+        Raises:
+            None
+        """
+        data = json.loads(body.decode())
+        # Save the code, input to a file
+        self.save_code(data["code"], data["language"])
+        # handle multiple input file
+        submission = {
+            "stdout": [],
+            "stderr": [],
+            "test_cases": data["test_cases"]
+        }
+        for code_input in data["input"]:
+            self.__save_input(code_input)
+            stdout, stderr = self.__execute(data["language"])
+            submission["stderr"].append(stderr)
+            submission["stdout"].append(stdout)
+        submission["status"] = "done execution"
+        # Send the output to judge
+        self.__judge(data["submission_id"])
+        # Save the result to submission database
+        self.redis_command(self.redis.set,
+                        data["submission_id"], json.dumps(submission), 600)
+        # Clean up
+        self.__clean_up(data["language"])
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def __judge(self, submission_id: str) -> int:
+        result = requests.post(
+            "http://judge.judge.svc.cluster.local/judge", json={"submission_id": submission_id})
+        return result.status_code
+
+    def __init_redis(self, environment):
+        if (environment == "development"):
+            redis_sentinels = os.getenv("REDIS_SENTINELS").strip()
+            redis_master_name = os.environ.get('REDIS_MASTER_NAME').strip()
+            redis_password = os.environ.get('REDIS_PASSWORD').strip()
+            redis_sentinel = Sentinel([(redis_sentinels, 5000)], socket_timeout=5)
+            redis_master = redis_sentinel.master_for(
+                redis_master_name, password=redis_password, socket_timeout=5)
+            return redis_master
+        elif (environment == "production"):
+            redis_master = redis.Redis(
+                host=os.getenv("REDIS_HOST").strip(), port=6379)
+            return redis_master
+
+    def __dind(self) -> docker.DockerClient:
+        max_retries = 4
+        count = 0
+        backoffSeconds = 2
+        while True:
+            try:
+                dind = docker.from_env()
+                # self.__load_interpreter(dind=dind, languages=self.languages)
+                return dind
+            except Exception as e:
+                count += 1
+                if count > max_retries:
+                    raise
+            time.sleep(backoffSeconds)
+
+    def redis_command(self, command, *args):
+        max_retries = 2
+        count = 0
+        backoffSeconds = 2
+        while True:
+            try:
+                return command(*args)
+            except (redis.exceptions.ConnectionError, redis.exceptions.TimeoutError):
+                count += 1
+                if count > max_retries:
+                    raise
+            print('Retrying in {} seconds'.format(backoffSeconds))
+            time.sleep(backoffSeconds)
+
+    def __init_rabbitmq(self, environment):
+        if (environment == "development"):
+            submission_queue = os.getenv("SUBMISSION_QUEUE").strip()
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=submission_queue))
+            channel = connection.channel()
+            return channel
+        elif (environment == "production"):
+            rabbitmq_url = self.__inject_username_password_to_rabbitmq_url(
+                os.getenv("SUBMISSION_QUEUE").strip())
+            parameters = pika.URLParameters(rabbitmq_url)
+            connection = pika.BlockingConnection(parameters)
+            channel = connection.channel()
+            return channel
+
+    def __inject_username_password_to_rabbitmq_url(rabbitmq_url, rabbitmq_username, rabbitmq_password):
+        username = rabbitmq_username.strip()
+        password = rabbitmq_password.strip()
+        rabbitmq_url = rabbitmq_url.replace(
+            "amqps://", "amqps://{}:{}@".format(username, password))
+        return rabbitmq_url
+
+    def __load_interpreter(self, dind: docker.DockerClient, languages: dict[str, str]):
+        images = [image.tags[0] for image in dind.images.list()]
+        try:
+            for language in languages:
+                if languages[language] not in images:
+                    dind.images.pull(languages[language])
+        except Exception as e:
+            raise e
+        return
+
+    def environment(callback):
+        def wrapper(*args, **kwargs):
+            try:
+
+                callback(*args, **kwargs)
+            except Exception as e:
+                raise e
+        return wrapper
 
 
-def judge(submission_id: int):
-    result = requests.post(
-        "http://judge.judge.svc.cluster.local/judge", json={"submission_id": submission_id})
-    return result.status_code
-
-
-def cleanup_stdout():
-    if os.path.exists("stdout.txt"):
-        os.remove("stdout.txt")
-
-
-def cleanup_stderr():
-    if os.path.exists("stderr.txt"):
-        os.remove("stderr.txt")
-
-
-def clean_up(langauge):
-    cleanup_stdout()
-    cleanup_stdout()
-    os.remove("input.txt")
-    if (langauge == "nodejs"):
-        os.remove("code.js")
-    elif (langauge == "python"):
-        os.remove("code.py")
-    else:
-        print("Language not supported")
-
-
-def save_code(code, language):
-    if (language == "python"):
-        with open("code.py", "w") as f:
-            f.write(code)
-    elif (language == "nodejs"):
-        with open("code.js", "w") as f:
-            f.write(code)
-    else:
-        print("Language not supported")
-
-
-def save_stderr(stderr):
-    with open("stderr.txt", "w") as f:
-        f.write(stderr)
-
-
-def save_stdout(stdout):
-    with open("stdout.txt", "w") as f:
-        f.write(stdout)
-
-
-def save_input(input):
-    with open("input.txt", "w") as f:
-        f.write(input)
-
-
-def read_file(filename):
-    with open(filename, "r") as f:
-        return f.read()
-
-
-def execute(language):
-    """
-    If return non-zero mean something wrong
-    """
-    try:
-        if (language == "nodejs"):
-            intrepreter = "node"
-            extension = ".js"
-        elif (language == "python"):
-            intrepreter = "python3"
-            extension = ".py"
-        result = subprocess.run(
-            [intrepreter, f"code{extension}"], capture_output=True, text=True, timeout=5, input=read_file("input.txt"))
-    except subprocess.TimeoutExpired:
-        result.returncode = 1
-        save_stderr("Time limit exceeded")
-        save_stdout("")
-    else:
-        save_stderr(result.stderr)
-        save_stdout(result.stdout)
-    return result.returncode
-
-
-queue_channel.basic_qos(prefetch_count=1)
-queue_channel.queue_declare(queue=os.getenv(
-    "CEE_INTERPRETER_QUEUE_NAME").strip(), durable=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
-queue_channel.basic_consume(queue='cee-intrepreter-queue',
-                            on_message_callback=callback)
-queue_channel.start_consuming()
+if __name__ == "__main__":
+    WORK_DIR = os.getenv("WORK_DIR")
+    NODEJS_CEE_INTERPRETER_IMAGE = os.getenv(
+        "NODEJS_CEE_INTERPRETER_IMAGE") or "node:16.13.0-alpine3.14"
+    PYTHON_CEE_INTERPRETER_IMAGE = os.getenv(
+        "PYTHON_CEE_INTERPRETER_IMAGE") or "python:alpine3.16"
+    worker = Worker(
+        envrionment=os.getenv("ENVIRONMENT"),
+        python=PYTHON_CEE_INTERPRETER_IMAGE,
+        nodejs=NODEJS_CEE_INTERPRETER_IMAGE)
+    print("initialize 1")
+    worker.run(os.getenv("CEE_INTERPRETER_QUEUE_NAME").strip())
+    print("initialize 2")
