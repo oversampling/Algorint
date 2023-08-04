e14d2c4976ea7a6de2c5b9cc5414e4b5f7473e17
chan1992241 <chan1992241@gmail.com>
2023-08-04 14:57:56 +0800

    CEE: Rewrite CEE engine

--- cee/go.mod
+++ cee/go.mod
@@ -8,6 +8,33 @@ require (
 )
 
 require (
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/docker/distribution v2.8.2+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_model v0.4.0 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.10.1 // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
+
+require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/docker/docker v24.0.5+incompatible
+	github.com/prometheus/client_golang v1.16.0
 )
--- cee/main.go
+++ cee/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"archive/tar"
+	"bytes"
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -9,11 +12,15 @@ import (
 	"strings"
 	"time"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/redis/go-redis/v9"
 )
 
+var supported_languages []string = strings.Split(os.Getenv("SUPPORTED_LANGUAGES"), ",")
 var submission_queue string = strings.TrimSpace(os.Getenv("SUBMISSION_QUEUE"))
 var rabbit_password string = strings.TrimSpace(os.Getenv("RABBITMQ_PASSWORD"))
 var rabbitmq_username string = strings.TrimSpace(os.Getenv("RABBMITMQ_USERNAME"))
@@ -31,8 +38,51 @@ var redis_client = redis.NewFailoverClient(&redis.FailoverOptions{
 	Password:      redis_password,
 })
 
+// language_details[language_name]["image"/"cmd"]
+var languages_details map[string]map[string]string = map[string]map[string]string{}
+
 func main() {
-	consume()
+	cli, err := Initialize_Docker_Client()
+	if err != nil {
+		log.Fatal("Failed to initialize docker client\n" + err.Error())
+	}
+	Initialize_Language_Executor(cli)
+	consume(cli)
+}
+
+func Initialize_Language_Executor(cli *client.Client) {
+	for _, language := range supported_languages {
+		language_details := strings.Split(language, "@")
+		log.Printf("Lanugage details: %s\n", language_details)
+		language_name := language_details[0]
+		language_image := language_details[1]
+		language_cmd := language_details[2]
+		language_extension := language_details[3]
+		languages_details[language_name] = map[string]string{
+			"image":     language_image,
+			"cmd":       language_cmd,
+			"extension": language_extension,
+		}
+		_, err := cli.ImagePull(context.Background(), language_image, types.ImagePullOptions{})
+		if err != nil {
+			log.Fatal("Failed to pull image\n" + err.Error())
+		}
+	}
+}
+
+func Initialize_Docker_Client() (*client.Client, error) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Printf("Failed to initialize docker client\n" + err.Error())
+		return nil, err
+	}
+	_, err = cli.Ping(ctx)
+	if err != nil {
+		log.Printf("Failed to ping docker client\n" + err.Error())
+		return nil, err
+	}
+	return cli, nil
 }
 
 func injectUsernamePasswordToRabbitMQURL(rabbitMQURL string, rabbitMQUsername string, rabbitMQPassword string) string {
@@ -42,7 +92,7 @@ func injectUsernamePasswordToRabbitMQURL(rabbitMQURL string, rabbitMQUsername st
 	return rabbitMQURL
 }
 
-func consume() {
+func consume(cli *client.Client) {
 	conn, err := Initiate_MQ_Client()
 	if err != nil {
 		log.Fatal("Failed to connect to RabbitMQ", err)
@@ -70,12 +120,12 @@ func consume() {
 		log.Fatal("Failed to register a consumer", err)
 	}
 	forever := make(chan bool)
-	go OnMessageReceived(msgs)
+	go OnMessageReceived(msgs, cli)
 	fmt.Println("Running...")
 	<-forever
 }
 
-func OnMessageReceived(msgs <-chan amqp.Delivery) {
+func OnMessageReceived(msgs <-chan amqp.Delivery, cli *client.Client) {
 	for d := range msgs {
 		log.Printf("Received a message: %s", d.Body)
 		submission_id, err := Get_Submission_Token_From_MQ(d.Body)
@@ -86,11 +136,47 @@ func OnMessageReceived(msgs <-chan amqp.Delivery) {
 		if err != nil {
 			log.Printf("Failed to get submission from redis\n" + err.Error())
 		}
-		_, err = Parse_Submission_From_Redis(result)
+		submission, err := Parse_Submission_From_Redis(result)
 		if err != nil {
 			log.Printf("Failed to parse submission from redis\n" + err.Error())
 		}
-		// log.Printf("Submission: %v", submission)
+		log.Printf("Submission: %v", submission)
+		for index, code_input := range submission.Input {
+			log.Printf("%v Processing input: %v", index, code_input)
+			// base64 decode code_input
+			code_input_decoded, err := base64.StdEncoding.DecodeString(code_input)
+			if err != nil {
+				log.Printf("Failed to decode input\n" + err.Error())
+			}
+			code, err := base64.StdEncoding.DecodeString(submission.Code)
+			if err != nil {
+				log.Printf("Failed to decode code\n" + err.Error())
+			}
+			for _, replaces := range submission.Replace[index] {
+				from, err := base64.StdEncoding.DecodeString(replaces.From)
+				if err != nil {
+					log.Printf("Failed to decode replace from\n" + err.Error())
+				}
+				to, err := base64.StdEncoding.DecodeString(replaces.To)
+				if err != nil {
+					log.Printf("Failed to decode replace to\n" + err.Error())
+				}
+				code = bytes.Replace(code, from, to, -1)
+			}
+			time_limit := submission.TimeLimit[index]
+			mem_limit := submission.MemoryLimit[index]
+			log.Printf("Code: %v", string(code))
+			log.Printf("Time Limit: %v", time_limit)
+			log.Printf("Memory Limit: %v", mem_limit)
+			log.Printf("Code Input: %v", string(code_input_decoded))
+			stdout, stderr, err := RunCode(*cli, code, code_input, submission.Language, time_limit, mem_limit)
+			if err != nil {
+				log.Printf("Failed to run code\n" + err.Error())
+			}
+			log.Printf("Stdout: %v", stdout)
+			log.Printf("Stderr: %v", stderr)
+
+		}
 		// Sleep 3 seconds in golang
 		time.Sleep(3 * time.Second)
 		log.Printf("Done processing submission: %s", submission_id)
@@ -195,4 +281,110 @@ func Initiate_Redis_Client() (*redis.Client, error) {
 		return nil, err
 	}
 	return client, nil
+}
+
+func RunCode(cli client.Client, code []byte, input string, language string, time_limit int, mem_limit_mb int) (string, string, error) {
+	ctx := context.Background()
+	// Create a container
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image:     languages_details[language]["image"],
+		Tty:       false, // False
+		OpenStdin: true,
+		// AttachStdin:  true,
+		// AttachStdout: true,
+		// AttachStderr: true,
+		WorkingDir: "/app",
+		// Cmd:        []string{"ls", "-la"},
+		Cmd:         []string{"python", "code.py"},
+		StopTimeout: &time_limit,
+	}, &container.HostConfig{
+		Resources: container.Resources{
+			Memory: int64(mem_limit_mb * 1024 * 1024),
+		},
+	}, nil, nil, "")
+	if err != nil {
+		log.Println("Error in creating container\n " + err.Error())
+		return "", "", err
+	}
+	// Copy code to container
+	content, err := Make_Archieve(fmt.Sprintf("code%s", languages_details[language]["extension"]), code)
+	if err != nil {
+		return "", "Error in making archieve", err
+	}
+	err = cli.CopyToContainer(ctx, resp.ID, "/app", content, types.CopyToContainerOptions{
+		AllowOverwriteDirWithFile: true,
+	})
+	if err != nil {
+		return "", "Error in copying file to container\n" + err.Error() + "\n", err
+	}
+	// Start container
+	err = cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
+	if err != nil {
+		return "", "Error in Starting container\n" + err.Error() + "\n", err
+	}
+	// Write input to container
+	waiter, err := cli.ContainerAttach(ctx, resp.ID, types.ContainerAttachOptions{
+		Stream: true,
+		Stdin:  true,
+		Stdout: true,
+		Stderr: true,
+	})
+	if err != nil {
+		return "", "Error in Attach stream from container", err
+	}
+	waiter.Conn.SetDeadline(time.Now().Add(time.Duration(time_limit) * time.Second))
+	_, err = waiter.Conn.Write([]byte(input))
+	if err != nil {
+		log.Println("Error in writing input to container\n " + err.Error())
+		return "", "", err
+	}
+	// Wait for container to finish
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			return "", "", err
+		}
+	case <-statusCh:
+	}
+	// Get container logs
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
+	if err != nil {
+		return "", "", err
+	}
+	defer out.Close()
+	stdoutput := new(bytes.Buffer)
+	stderror := new(bytes.Buffer)
+	_, err = stdcopy.StdCopy(stdoutput, stderror, out)
+	if err != nil {
+		return "", "", err
+	}
+	return stdoutput.String(), stderror.String(), nil
+}
+
+func Make_Archieve(filename string, data []byte) (*bytes.Reader, error) {
+	var buf bytes.Buffer
+	tarWriter := tar.NewWriter(&buf)
+	tarHeader := &tar.Header{
+		Name: filename,
+		Mode: 0777,
+		Size: int64(len(data)),
+	}
+	err := tarWriter.WriteHeader(tarHeader)
+	if err != nil {
+		log.Printf("Error in writing tar header\n")
+		return nil, err
+	}
+	_, err = tarWriter.Write(data)
+	if err != nil {
+		log.Printf("Error in writing tar data\n")
+		return nil, err
+	}
+	err = tarWriter.Close()
+	if err != nil {
+		log.Printf("Error in closing tar writer\n")
+		return nil, err
+	}
+	content := bytes.NewReader(buf.Bytes())
+	return content, nil
 }
