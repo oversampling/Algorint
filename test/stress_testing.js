da57744d9f0e19cdaa109946fe784e85f9efdd33
chan1992241 <chan1992241@gmail.com>
2023-08-29 14:45:54 +0800

    Test: K6 Load Testing Script

---
+++ test/stress_testing.js
@@ -0,0 +1,29 @@
+import http from "k6/http";
+import { group, sleep, check } from "k6";
+import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.2.0/index.js";
+
+export const options = {
+  discardResponseBodies: true,
+  stages: [
+    { duration: "54s", target: 4 }, // traffic ramp-up from 1 to 100 users over 5 minutes.
+    { duration: "30s", target: 8 }, // stay at 100 users for 30 minutes
+    { duration: "30s", target: 14 }, // ramp-down to 0 users
+    { duration: "66s", target: 20 }, // ramp-down to 0 users
+    { duration: "0s", target: 0 }, // ramp-down to 0 users
+  ],
+};
+
+export default function () {
+  group("CEE Load Testing", function () {
+    const response = http.post(
+      `http://${__ENV.MY_HOSTNAME}/api/assignment/evaluate`
+    );
+    console.log(
+      `VU: ${__VU}  -  ITER: ${__ITER}  -  ${new Date().toLocaleString()}`
+    );
+    check(response, {
+      "is status 200": (r) => r.status === 200,
+    });
+    sleep(randomIntBetween(2, 5));
+  });
+}
